a38b683c19f64caf4511b241bedca4263b80e44a
exxusio <vladislav.yakushev03@mail.ru>
2024-10-20 20:22:12 +0300

    feat: added Repository interfaces

---
+++ src/EventsWebApplication.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace EventsWebApplication.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TEntity>> GetByPredicateAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        Task AddAsync(TEntity item, CancellationToken cancellationToken = default);
+        Task Delete(TEntity item);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/EventsWebApplication.Domain/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,8 @@
+using EventsWebApplication.Domain.Entities;
+
+namespace EventsWebApplication.Domain.Interfaces.Repositories
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+    }
+}
