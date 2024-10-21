4dd9f59efe5f5d7fbbfa27c876cbe7549718a698
exxusio <vladislav.yakushev03@mail.ru>
2024-10-21 18:36:54 +0300

    refact: remove Name property from EventCriteriaDto

--- src/EventsWebApplication.Application/DTOs/Events/EventCriteriaDto.cs
+++ src/EventsWebApplication.Application/DTOs/Events/EventCriteriaDto.cs
@@ -4,7 +4,6 @@ namespace EventsWebApplication.Application.DTOs.Events
 {
     public class EventCriteriaDto
     {
-        public string Name { get; set; }
         public DateTime? Date { get; set; }
         public string? Location { get; set; }
         public EventCategoryReadDto? Category { get; set; }
