c27ea60935db4b822635ff437d3c65701561ccf8
exxusio <vladislav.yakushev03@mail.ru>
2024-10-28 19:45:05 +0300

    refact: make EventFilter properties nullable

--- src/EventsWebApplication.Domain/Filters/EventFilter.cs
+++ src/EventsWebApplication.Domain/Filters/EventFilter.cs
@@ -2,10 +2,10 @@ namespace EventsWebApplication.Domain.Filters
 {
     public class EventFilter
     {
-        public string SearchTerm { get; set; }
+        public string? SearchTerm { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string Location { get; set; }
+        public string? Location { get; set; }
         public Guid? CategoryId { get; set; }
     }
 }
