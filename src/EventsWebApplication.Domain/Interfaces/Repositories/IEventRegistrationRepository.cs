82f86616c6aae5d15d1d9cd9f80cfd913438ea5a
exxusio <vladislav.yakushev03@mail.ru>
2024-10-28 14:56:48 +0300

    feat: added specific query methods to repository interfaces

--- src/EventsWebApplication.Domain/Interfaces/Repositories/IEventRegistrationRepository.cs
+++ src/EventsWebApplication.Domain/Interfaces/Repositories/IEventRegistrationRepository.cs
@@ -4,5 +4,8 @@ namespace EventsWebApplication.Domain.Interfaces.Repositories
 {
     public interface IEventRegistrationRepository : IRepository<EventRegistration>
     {
+        Task<EventRegistration?> GetRegistrationByEventIdAndParticipantId(Guid eventId, Guid userId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<EventRegistration>> GetRegistrationsByParticipantId(Guid userId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<EventRegistration>> GetRegistrationsByEventId(Guid eventId, CancellationToken cancellationToken = default);
     }
 }
--- src/EventsWebApplication.Domain/Interfaces/Repositories/IRoleRepository.cs
+++ src/EventsWebApplication.Domain/Interfaces/Repositories/IRoleRepository.cs
@@ -4,5 +4,6 @@ namespace EventsWebApplication.Domain.Interfaces.Repositories
 {
     public interface IRoleRepository : IRepository<Role>
     {
+        Task<Role?> GetRoleByName(string name, CancellationToken cancellationToken = default);
     }
 }
