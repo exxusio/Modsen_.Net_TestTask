3ce182bafa858665e78db631fac0764136bc6dd4
exxusio <vladislav.yakushev03@mail.ru>
2024-10-28 15:14:23 +0300

    feat: added filters

---
+++ src/EventsWebApplication.Domain/Filters/EventFilter.cs
@@ -0,0 +1,11 @@
+namespace EventsWebApplication.Domain.Filters
+{
+    public class EventFilter
+    {
+        public string SearchTerm { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Location { get; set; }
+        public Guid? CategoryId { get; set; }
+    }
+}
---
+++ src/EventsWebApplication.Domain/Filters/PagedFilter.cs
@@ -0,0 +1,8 @@
+namespace EventsWebApplication.Domain.Filters
+{
+    public class PagedFilter
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
