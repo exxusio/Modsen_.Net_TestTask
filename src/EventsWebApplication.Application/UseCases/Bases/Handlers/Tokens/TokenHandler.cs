1715b4aac7542d1b9ff5d870005f279a100bc992
exxusio <vladislav.yakushev03@mail.ru>
2024-10-29 20:38:16 +0300

    feat: add TokenHandler class for token generation and storage

---
+++ src/EventsWebApplication.Application/UseCases/Bases/Handlers/Tokens/TokenHandler.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using EventsWebApplication.Application.Algorithms.Interfaces;
+using EventsWebApplication.Application.DTOs.Tokens;
+using EventsWebApplication.Domain.Entities;
+using EventsWebApplication.Domain.Interfaces;
+
+namespace EventsWebApplication.Application.UseCases.Bases.Handlers.Tokens
+{
+    public abstract class TokenHandler
+    {
+        protected readonly ITokensGenerator _tokenGenerator;
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IMapper _mapper;
+
+        protected TokenHandler(ITokensGenerator tokenGenerator, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _tokenGenerator = tokenGenerator;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        protected async Task<TokensResponse> GenerateAndSaveTokens(User user, CancellationToken cancellationToken)
+        {
+            var token = _tokenGenerator.GenerateRefreshToken();
+
+            var refreshToken = _mapper.Map<RefreshToken>(token);
+            refreshToken.User = user;
+
+            var refreshTokenRepository = _unitOfWork.GetRepository<RefreshToken>();
+
+            await refreshTokenRepository.AddAsync(refreshToken, cancellationToken);
+            await refreshTokenRepository.SaveChangesAsync(cancellationToken);
+
+            var tokenResponse = new TokensResponse
+            {
+                AccessToken = _tokenGenerator.GenerateAccessToken(refreshToken.User),
+                RefreshToken = token
+            };
+
+            return tokenResponse;
+        }
+    }
+}
