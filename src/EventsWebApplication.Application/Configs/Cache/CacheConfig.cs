75649bd60444c5646093b7eb09af0f77b5343093
exxusio <vladislav.yakushev03@mail.ru>
2024-10-30 21:11:49 +0300

    feat: add CacheConfig class for caching configuration

---
+++ src/EventsWebApplication.Application/Configs/Cache/CacheConfig.cs
@@ -0,0 +1,7 @@
+namespace EventsWebApplication.Application.Configs.Cache
+{
+    public static class CacheConfig
+    {
+        public static readonly TimeSpan EVENT_TIME = TimeSpan.FromMinutes(120);
+    }
+}
