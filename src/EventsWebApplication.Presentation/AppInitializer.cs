9485185f676b9be4903bcef539d4088292e59d9a
exxusio <vladislav.yakushev03@mail.ru>
2024-11-06 19:43:17 +0300

    feat: add application startup and presentation layer configuration

---
+++ src/EventsWebApplication.Presentation/AppInitializer.cs
@@ -0,0 +1,64 @@
+using EventsWebApplication.Application.Abstractions.Data;
+using EventsWebApplication.Application.Abstractions.Auth;
+using EventsWebApplication.Infrastructure.Data;
+using EventsWebApplication.Infrastructure;
+using EventsWebApplication.Presentation.Middlewares;
+
+namespace EventsWebApplication.Presentation
+{
+    public static class AppInitializer
+    {
+        public static async Task<WebApplication> StartApplication(this WebApplication webApplication)
+        {
+            webApplication.UseRouting();
+
+            webApplication.UseAuthentication();
+            webApplication.UseAuthorization();
+
+            webApplication.UseMiddleware<ExceptionHandlingMiddleware>();
+
+            webApplication.UseHttpsRedirection();
+
+            webApplication.MapControllers();
+
+            await webApplication.DbInitialize();
+            await webApplication.SeedDataAsync();
+
+            webApplication.SwaggerStart();
+
+            webApplication.Run();
+
+            return webApplication;
+        }
+
+        private static async Task<WebApplication> DbInitialize(this WebApplication webApplication)
+        {
+            using var scope = webApplication.Services.CreateScope();
+            var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await DbInitializer.Initialize(appDbContext);
+
+            return webApplication;
+        }
+
+        private static async Task<WebApplication> SeedDataAsync(this WebApplication webApplication)
+        {
+            using var scope = webApplication.Services.CreateScope();
+            var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+            var passwordHasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
+            await DbInitializer.SeedAsync(unitOfWork, passwordHasher);
+
+            return webApplication;
+        }
+
+        private static WebApplication SwaggerStart(this WebApplication webApplication)
+        {
+            if (webApplication.Environment.IsDevelopment())
+            {
+                webApplication.UseSwagger();
+                webApplication.UseSwaggerUI();
+            }
+
+            return webApplication;
+        }
+    }
+}
---
+++ src/EventsWebApplication.Presentation/Program.cs
@@ -0,0 +1,12 @@
+using EventsWebApplication.Application;
+using EventsWebApplication.Infrastructure;
+using EventsWebApplication.Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
+
+var app = builder.Build();
+await app.StartApplication();
