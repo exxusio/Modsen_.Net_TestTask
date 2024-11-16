fa73116726d5f83daa00d564e7e20e75fc0f9c2a
exxusio <vladislav.yakushev03@mail.ru>
2024-11-16 18:19:43 +0300

    feat: update validators

--- frontend/src/app/helpers/custom-validators.ts
+++ frontend/src/app/helpers/custom-validators.ts
@@ -20,4 +20,53 @@ export class CustomValidators {
             return { notEqual: true };
         };
     }
+
+    static dateValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const dateRegex =
+                /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/;
+            const isValidFormat = dateRegex.test(control.value);
+
+            if (!isValidFormat) {
+                return {
+                    invalidDateFormat: true,
+                };
+            }
+
+            const [day, month, year] = control.value.split('.').map(Number);
+            const inputDate = new Date(year, month - 1, day);
+            const currentDate = new Date();
+
+            if (inputDate > currentDate) {
+                return { dateTooFuture: true };
+            }
+
+            if (
+                inputDate.getDate() !== day ||
+                inputDate.getMonth() + 1 !== month ||
+                inputDate.getFullYear() !== year
+            ) {
+                return { invalidDate: true };
+            }
+
+            return null;
+        };
+    }
+
+    static futureDateValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const currentDate = new Date();
+            const inputDate = new Date(control.value);
+
+            if (
+                control.value &&
+                inputDate.setHours(0, 0, 0, 0) <=
+                    currentDate.setHours(0, 0, 0, 0)
+            ) {
+                return { pastDate: true };
+            }
+
+            return null;
+        };
+    }
 }
